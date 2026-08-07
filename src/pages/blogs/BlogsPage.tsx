a57e851fbfe23cf30483b8e5e254dd80929b32e9
Anbu-Elumalai <anbuelumalai952002@gmail.com>
2026-08-07 18:13:56 +0530

    production changes

--- src/pages/blogs/BlogsPage.tsx
+++ src/pages/blogs/BlogsPage.tsx
@@ -41,8 +41,8 @@ const slugify = (text: string) => {
     .trim()
     .replace(/\s+/g, "-") // Replace spaces with -
     .replace(/&/g, "-and-") // Replace & with 'and'
-    .replace(/[^\w\-]+/g, "") // Remove all non-word characters
-    .replace(/\-\-+/g, "-") // Replace multiple - with single -
+    .replace(/[^\w-]+/g, "") // Remove all non-word characters
+    .replace(/-+/g, "-") // Replace multiple - with single -
     .replace(/^-+/, "") // Trim - from start of text
     .replace(/-+$/, ""); // Trim - from end of text
 };
